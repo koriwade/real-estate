f6ddb059b822ee103e0f22b938b41b061fff54d9
Kori Wade <koriwade@Creative104.local>
2016-10-19 10:07:18 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(){
+    $(".hero-text").css("opacity", 1 - $(window).scrollTop() / 450);
+  });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
